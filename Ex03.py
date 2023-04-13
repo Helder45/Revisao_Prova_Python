d2223a3f4b4a82efd1a66b91372cb3b34a0cbb26
Helder45 <helder.pereira@estudante.ifms.edu.br>
2023-04-13 15:47:16 -0400

    Terceiro exercicio completo. Criado o quarto.

---
+++ Ex03.py
@@ -0,0 +1,14 @@
+def contar_caracteres(string):
+    dicionario = {}
+    for char in string:
+        if char in dicionario:
+            dicionario[char] += 1
+        else:
+            dicionario[char] = 1
+    return dicionario
+
+
+palavra = input("Digite uma palavra para contar cada caractere nela: ")
+string = "banana"
+contagem = contar_caracteres(palavra)
+print(contagem)
